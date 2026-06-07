0677c48fef3af96ced3c052d3ef5d41e08f57b64
Almir Sarajčić <almir.sarajcic@icloud.com>
2026-06-07 19:14:21 +0200

    Remove unused live and context generators

--- phx.gen.context/context.ex
+++
@@ -1,8 +0,0 @@
-defmodule <%= inspect context.module %> do
-  @moduledoc """
-  The <%= context.name %> context.
-  """
-
-  import Ecto.Query, warn: false
-
-end
--- phx.gen.context/context_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule <%= inspect context.module %>Test do
-  use <%= inspect context.base_module %>.DataCase, async: true
-
-end
